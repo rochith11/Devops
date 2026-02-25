be3cc6fb12ec944843234eea694e86063f44bd33
rochith11 <rochith.s.phutane10@gmail.com>
2026-02-25 14:55:18 +0530

    Initial Commit

---
+++ src/main/java/com/example/MainClass.java
@@ -0,0 +1,14 @@
+package com.example;
+
+/**
+*Hello World!
+*
+*/
+public class MainClass 
+	{
+	public static void main(String[] args) 
+		{
+		System.out.println("Hi BIT, how are you all");
+		}
+	}
+
